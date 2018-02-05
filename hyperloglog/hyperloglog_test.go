0f71321cd56078b182a0f614319de2464cbb46cd
kiran <me@kiranbot.com>
2018-02-05 01:28:54 -0800

    work out binary arithmetic for hyperloglog

--- hyperloglog/hyperloglog_test.go
+++ hyperloglog/hyperloglog_test.go
@@ -1,7 +1,30 @@
-package hyperloglog_test
+package hyperloglog
 
-import "testing"
+import (
+	"testing"
 
-func TestReverse(t *testing.T) {
-	return
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCountLeadingZeros(t *testing.T) {
+	h, _ := Initialize(8)
+	assert.Equal(t, uint8(0), h.countLeadingZeros(0x91<<56))
+	assert.Equal(t, uint8(0), h.countLeadingZeros(0xab<<56))
+	assert.Equal(t, uint8(1), h.countLeadingZeros(0x48<<56))
+	assert.Equal(t, uint8(2), h.countLeadingZeros(0x28<<56))
+	assert.Equal(t, uint8(3), h.countLeadingZeros(0x18<<56))
+	assert.Equal(t, uint8(4), h.countLeadingZeros(0x09<<56))
+	assert.Equal(t, uint8(5), h.countLeadingZeros(0x04<<56))
+}
+
+func TestSplitWord(t *testing.T) {
+	h, _ := Initialize(16)
+
+	i, remainder := h.splitWord(0x1111222233334444)
+	assert.Equal(t, uint64(0x1111), i)
+	assert.Equal(t, uint64(0x2222333344440000), remainder)
+
+	i, remainder = h.splitWord(0x0123456789abcdef)
+	assert.Equal(t, uint64(0x0123), i)
+	assert.Equal(t, uint64(0x456789abcdef0000), remainder)
 }
--- hyperloglog/hyperloglogplus.go
+++ hyperloglog/hyperloglogplus.go
@@ -1,8 +1,7 @@
 package hyperloglog
 
 import (
-	"encoding/binary"
-	"fmt"
+	"errors"
 	"hash/fnv"
 	"math"
 )
@@ -16,23 +15,34 @@ import (
 // HyperLogLog estimates cardinality
 type HyperLogLog struct {
 	buckets     []byte // byte array of buckets
-	bucketCount uint32 // number of buckets
+	bucketCount uint64 // number of buckets
 	precision   uint8  // precision (number of bytes to count)
+	debug       bool
 }
 
-// ProcessNumber adds another number to the HyperLogLog structure
+// Initialize takes a number of bytes to initialize with,
+// and returns the struct
+func Initialize(precision uint8) (*HyperLogLog, error) {
+	hll := &HyperLogLog{}
+	if precision > 16 || precision < 4 {
+		return nil, errors.New("precision must be between 4 and 16")
+	}
+	hll.precision = precision
+	hll.bucketCount = 1 << precision
+	hll.buckets = make([]byte, hll.bucketCount)
+	return hll, nil
+}
+
+// ProcessElement adds another number to the HyperLogLog structure
 func (hll *HyperLogLog) ProcessElement(value string) {
 	// hash the number
 	h := fnv.New64a()
 	h.Write([]byte(value))
 	hashedNumber := h.Sum64()
-	fmt.Printf("%+v\n", hashedNumber)
-	// slice the number --
-	hashedBytes := convertToBytes(hashedNumber)
 	// a. figure out which bucket to put it into
-	bucket := hll.getBucket(hashedBytes)
+	bucket, remainder := hll.splitWord(hashedNumber)
 	// b. count the longest run of leading zeroes in the second half
-	contiguousZeros := hll.countZeros(hashedBytes)
+	contiguousZeros := hll.countLeadingZeros(remainder)
 
 	if contiguousZeros > hll.buckets[bucket] {
 		hll.buckets[bucket] = contiguousZeros
@@ -40,22 +50,32 @@ func (hll *HyperLogLog) ProcessElement(value string) {
 	return
 }
 
-func convertToBytes(hashedNumber uint64) []byte {
-	bs := make([]byte, 64)
-	binary.LittleEndian.PutUint64(bs, hashedNumber)
-	fmt.Println(bs)
-	return bs
-}
-
-func (hll *HyperLogLog) getBucket(hashedBytes []byte) int {
+// splitWord, given a hashedNumber, extracts the leading bits to determine
+// which bucket the number should contribute to
+func (hll *HyperLogLog) splitWord(hashedNumber uint64) (uint64, uint64) {
 	// given a hashed number, figure out which bucket it goes into
 
-	return 0
+	// shift the hashed number over until precision bits are left
+	// then mask out the rest of the number
+	i := (hashedNumber >> (64 - hll.precision)) & (hll.bucketCount - 1)
+
+	// the remainder --
+	remainder := (hashedNumber << hll.precision)
+
+	return i, remainder
 }
 
-func (hll *HyperLogLog) countZeros(hashedBytes []byte) uint8 {
+func (hll *HyperLogLog) countLeadingZeros(remainder uint64) uint8 {
 	// given a hashed number, figure out how many leading zeros it has
-	return 0
+	var mask uint64
+	mask = 1 << 63
+	clz := uint8(0)
+	// limit = 1 << (hll.p - 1)
+	for (remainder & mask) == 0 {
+		mask = mask >> 1
+		clz++
+	}
+	return clz
 }
 
 // Cardinality emits the current estimated cardinality
@@ -69,25 +89,8 @@ func (hll *HyperLogLog) Cardinality() (cardinality int) {
 	return 0
 }
 
-// Initialize takes a number of bytes to initialize with,
-// and returns the struct
-func Initialize(precision uint8) *HyperLogLog {
-	hll := &HyperLogLog{}
-	// todo: is this cast safe?
-	hll.precision = precision
-	hll.bucketCount = 1 << precision
-	hll.buckets = make([]byte, hll.bucketCount)
-	return hll
-}
-
 // EstimatedError returns the approximate precision
 func (hll *HyperLogLog) EstimatedError() (est float64) {
 	// the standard error for an HLL with n registers is less than 1.04/sqrt(m).
 	return 1.04 / (math.Sqrt(float64(hll.bucketCount)))
-}
-
-func main() {
-	h := Initialize(3)
-	h.ProcessElement("kittens")
-	h.ProcessElement("elephants")
 }
